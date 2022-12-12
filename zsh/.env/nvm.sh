c494b792f049c862b8b0825f149e9e1c3ebfda3c
groveer <groveer@uniontech.com>
2022-12-12 10:36:27 +0800

    refactor: nvm

--- zsh/.env/nvm.sh
+++ zsh/.env/nvm.sh
@@ -1,3 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Archlinux 中安装nvm包
+sh='/usr/share/nvm/init-nvm.sh'
+[ -f ${sh} ] && source ${sh}
