6fd6011e725d47572cfb659069c0c360885341b5
groveer <groveer@outlook.com>
2022-12-13 09:40:16 +0800

    chore(zsh): proxy use wsl

--- zsh/.env/proxy.sh
+++ zsh/.env/proxy.sh
@@ -1,3 +1,6 @@
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=$http_proxy
-export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+# proxy set 设置代理
+# proxy unset 取消代理
+# proxy test 查看代理情况
+# 因本人使用 WSL2，使用此脚本可方便设置主机代理，非 WSL2 用户可删除文件：rm -f proxy*
+alias proxy="source $(dirname $(readlink -f $0))/proxy-script"
+#proxy set
